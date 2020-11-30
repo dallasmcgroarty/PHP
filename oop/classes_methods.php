c73389e3e18804f5285b444f5919101df0e29ce5
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-11-29 23:04:45 -0800

    oop

--- oop/classes_methods.php
+++ oop/classes_methods.php
@@ -1,20 +1,28 @@
 <?php 
 
 class User {
-    public function __construct(){
-        echo 'Constructor Called';
+
+    private $id;
+    private $username;
+    private $email;
+    private $password;
+
+    public function __construct($username, $password){
+        //echo 'Constructor Called';
+        $this->username = $username;
+        $this->password = $password;
     }
 
     public function register(){
         echo 'User registered.';
     }
 
-    public function login($username, $password='None') {
-        $this->auth_user($username,$password);
+    public function login() {
+        $this->auth_user();
     }
 
-    public function auth_user($username, $password) {
-        echo $username . ' is authenticated';
+    public function auth_user() {
+        echo $this->username . ' is authenticated';
     }
 
     public function __destruct(){
@@ -22,11 +30,34 @@ class User {
     }
 }
 
-$user = new User;
+$user = new User('Tom','1234');
 echo '<br>';
 $user->register();
 echo '<br>';
-$user->login('Tom','1234');
+$user->login();
 echo '<br>';
 
+class Post{
+    private $name;
+
+    public function __set($name,$value){
+        echo 'Setting ' . $name . ' to <strong>' . $value . '</strong><br/>';
+        $this->name = $value;
+    }
+
+    public function __get($name) {
+        echo 'Getting ' . $name . ' <strong>' .$this->name . '</strong><br/>';
+    }
+
+    public function __isset($name) {
+        echo 'Is ' . $name . ' set?<br />';
+        return isset($this->name);
+    }
+}
+
+$post = new Post;
+$post->name = 'Testing';
+echo $post->name;
+
+var_dump(isset($post->name));
 ?>
